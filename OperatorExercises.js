c9b244a5b2fc05969b1e8e969cc0adcba7be169e
unknown <GrahamE@ywcanwil.org>
2023-04-05 14:34:41 -0500

    Exercises to complete work on today

---
+++ OperatorExercises.js
@@ -0,0 +1,22 @@
+// * Operator Exercises
+// * Please log all exercises to the console unless stated otherwise
+
+// TODO Add 200 and 7
+
+// TODO Subtract 15 and 1
+
+// TODO Multiply 10 by 5
+
+// TODO Divide 10 by 2
+
+// TODO Modulo Divide 100 by 2
+// ! The result of Modulus Division is the remainder, rather than the dividend like with normal division
+// ! When spoken out loud, we usually say 100 "Mod" 2
+
+// TODO Take 5 to the power of 2 (Exponent)
+
+// TODO Increment by one (There are two ways of doing this)
+let incrementMe = 10;
+
+// TODO Decrement by two (There are two ways of doing this)
+let decrementMe = 10;
---
+++ VariableExercises.js
@@ -0,0 +1,25 @@
+// * Module 2 Exercises
+
+// TODO Create a variable called firstName and another called lastname
+// TODO Use console.log() to output "Hi, my name is [firstname] [lastName]"
+
+// ! You can use a backtick `` string to insert variables into a string
+// ! Do this by using this notation `String string string ${yourVariableHere}`
+
+
+// TODO Create a variable called num1 and assign it a value of 10
+// TODO Create a variable called num2 and assign it a value of 5
+// TODO Use console.log() to log their sum
+
+
+
+// TODO Create a variable x and give it a value of 10. Then, set x to 5.
+// TODO Console.log() the output to see which value it takes
+
+
+// TODO Create a constant called y and assign a value of 15.
+// TODO Reassign it, then console.log() y. What happens?
+
+
+// TODO 
+
