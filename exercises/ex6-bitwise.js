49744c8f94bf9d705e1a2892432c36495505b486
Graham Eichsteadt <GrahamE@ywcanwil.org>
2023-08-01 15:50:09 -0500

    Updated bitwise

---
+++ exercises/ex6-bitwise.js
@@ -0,0 +1,36 @@
+// * Bitwise Exercises
+
+/**
+ * Use console.log() to find the value of 5 & 9.
+ */
+function bitwiseAnd() {
+    // TODO Write your answer here
+}
+
+/**
+ * Use console.log() to find the value of 12 | 7.
+ */
+function bitwiseOr() {
+    // TODO Write your answer here
+}
+
+/**
+ * Use console.log() to find the value of ~10.
+ */
+function bitwiseNot() {
+    // TODO Write your answer here
+}
+
+/**
+ * Use console.log() to find the value of 28 << 2.
+ */
+function bitwiseLeftShift() {
+    // TODO Write your answer here
+}
+
+/**
+ * Use console.log() to find the value of 28 << 2.
+ */
+function bitwiseRightShift() {
+    // TODO Write your answer here
+}
